55457a248b170e05f19403dd39f94150ec408d25
mi1212 <mc_gorba4ev@mail.ru>
2022-09-12 14:27:50 +0300

    added project

---
+++ newsApp/newsApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  newsApp
+//
+//  Created by Mikhail Chuparnov on 12.09.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
